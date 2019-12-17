6bc55c5f390df5fd6663a63111b4f0844adee547
nix <zhuzhuxinxin@gmail.com>
2019-12-17 16:50:25 +0700

    a1

---
+++ precache-manifest.f20bfdd93a43b8e392e1186c73a30393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7700ac6606a947359bed",
+    "url": "/css/app.eb1fb7f4.css"
+  },
+  {
+    "revision": "528f8b60c189cfc02f124f2cb754818d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7700ac6606a947359bed",
+    "url": "/js/app.9a4603dc.js"
+  },
+  {
+    "revision": "1b8cbe644d4e20f00caa",
+    "url": "/js/chunk-vendors.28cd79af.js"
+  },
+  {
+    "revision": "a150d3e16ff2a2966b69e46fa913ef72",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
